ca3e4968d41c21ecaceb308aab64cdb5f1a964f3
Aly Badawy <AlyBadawy@users.noreply.github.com>
2022-07-19 20:02:55 -0400

    Initial Setup. (#2)
    * Setup
    * Setup CORS
    * Setup dotENV
    * More Setup
    * More Setup
    
    closes #1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::API
+  include JSONAPI::Deserialization
+
+  before_action :underscore_params!
+
+  private
+
+  def jsonapi_meta(resources)
+    meta = { provider: ENV.fetch("API_PROVIDER") }
+    meta[:total] = resources.respond_to?(:count) ? resources.count : 1
+
+    meta
+  end
+
+  def underscore_params!
+    params.deep_transform_keys! do |key|
+      ["Access-Token", "Refresh-Token"].include?(key) ? key : key.underscore
+    end
+  end
 end
